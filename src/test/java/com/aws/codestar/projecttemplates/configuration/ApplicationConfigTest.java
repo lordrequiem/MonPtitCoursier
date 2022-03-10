5d936038f4c2fd384f0d8a9a5f9ec1304965c17b
lordrequiem <earthwind@hotmail.fr>
2022-03-10 22:02:53 +0100

    Creation classe de test de la config

---
+++ src/test/java/com/aws/codestar/projecttemplates/configuration/ApplicationConfigTest.java
@@ -0,0 +1,13 @@
+package com.aws.codestar.projecttemplates.configuration;
+
+import org.junit.jupiter.api.DisplayName;
+
+import com.aws.codestar.projecttemplates.configuration.ApplicationConfig;
+
+/**
+ * Tests for {@link ApplicationConfig}. Modify the tests in order to support your use case as you build your project.
+ */
+@DisplayName("Tests for ApplicationConfig")
+public class ApplicationConfigTest {
+
+}
