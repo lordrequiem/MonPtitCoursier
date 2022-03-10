a50a8c9b7eda26bcf3153fb25bd09d4ba570cf8b
lordrequiem <earthwind@hotmail.fr>
2022-03-10 23:27:59 +0100

    Correction erreurs yaml, config eclipse et test du changement de texte

--- src/main/java/com/aws/codestar/projecttemplates/configuration/MvcConfig.java
+++ src/main/java/com/aws/codestar/projecttemplates/configuration/MvcConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 
 /**
